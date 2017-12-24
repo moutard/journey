abd8a018b3e4ad07072be673b42b183db524355e
Raphael Moutard <moutardr@amazon.com>
2017-12-24 16:12:03 +0100

    Remove useless images and reduce size of images for speed insights

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp         = require("gulp"),
     sass         = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer")
+    autoprefixer = require("gulp-autoprefixer"),
+    htmlmin      = require('gulp-htmlmin');
 
 // Compile SCSS files to CSS
 gulp.task("scss", function () {
@@ -12,12 +13,20 @@ gulp.task("scss", function () {
             browsers : ["last 20 versions"]
         }))
         .pipe(gulp.dest("static/css"))
-})
+});
+
+gulp.task('minify', function() {
+  return gulp.src('public/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('public'));
+});
+
 
 // Watch asset folder for changes
-gulp.task("watch", ["scss"], function () {
-    gulp.watch("src/scss/**/*", ["scss"])
-})
+gulp.task("watch", ["scss", "minify"], function () {
+    gulp.watch("src/scss/**/*", ["scss"]);
+    gulp.watch("public/*.html", ["minify"]);
+});
 
 // Set watch as default task
 gulp.task("default", ["watch"])
